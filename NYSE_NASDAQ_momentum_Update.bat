0efd930f497b8efefe710ca4283c642df19740eb
tonoma11111 <selelon2000@hotmail.co.jp>
2025-03-09 07:14:43 +0900

    Update commit

---
+++ NYSE_NASDAQ_momentum_Update.bat
@@ -0,0 +1,13 @@
+echo yfinanceデータ取得開始
+cd C:\Users\tonoma\.virtualenvs\kabu-ySnafzVC\Scripts
+python.exe C:\work\kabu\Momentum_NYSE_NASDAQ\momentum_calculator.py
+timeout /t 7000 /nobreak
+echo データ取得完了
+
+echo git push開始
+cd C:\work\kabu\Momentum_NYSE_NASDAQ
+git add .
+git commit -m "Update commit"
+git push -u origin_momentum_nyse_nasdaq main
+echo git push完了
+pause
