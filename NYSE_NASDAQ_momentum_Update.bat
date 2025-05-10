60d3e72fc58f63439b5689f54ccd0f646760dff7
Tonomasan <tonoma111@gmail.com>
2025-05-10 17:05:15 +0900

    Update commit

--- NYSE_NASDAQ_momentum_Update.bat
+++ NYSE_NASDAQ_momentum_Update.bat
@@ -4,7 +4,7 @@ rem C:\work\kabu\Momentum_NYSE_NASDAQ_HTML\Mom_US\Scripts\python.exe C:\work\kab
 rem echo データ取得完了
 
 echo git push開始
-cd /d C:\work\kabu\Momentum_NYSE_NASDAQ_HTML
+cd C:\work\kabu\Momentum_NYSE_NASDAQ_HTML
 "C:\Program Files\Git\cmd\git.exe" add .
 "C:\Program Files\Git\cmd\git.exe" commit -m "Update commit"
 "C:\Program Files\Git\cmd\git.exe" push -u origin_momentum_nyse_nasdaq main
