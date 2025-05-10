99e1d3b476376e5037ea700836bf6de1fe227e1b
Tonomasan <tonoma111@gmail.com>
2025-05-10 17:03:33 +0900

    Update commit

--- NYSE_NASDAQ_momentum_Update.bat
+++ NYSE_NASDAQ_momentum_Update.bat
@@ -1,14 +1,14 @@
 rem @echo off
-rem echo yfinanceãƒ‡ãƒ¼ã‚¿å–å¾—é–‹å§‹
+rem echo yfinanceƒf[ƒ^æ“¾ŠJn
 rem C:\work\kabu\Momentum_NYSE_NASDAQ_HTML\Mom_US\Scripts\python.exe C:\work\kabu\Momentum_NYSE_NASDAQ_HTML\momentum_calculator.py
-rem echo ãƒ‡ãƒ¼ã‚¿å–å¾—å®Œäº†
+rem echo ƒf[ƒ^æ“¾Š®—¹
 
-echo git pushé–‹å§‹
+echo git pushŠJn
 cd /d C:\work\kabu\Momentum_NYSE_NASDAQ_HTML
 "C:\Program Files\Git\cmd\git.exe" add .
 "C:\Program Files\Git\cmd\git.exe" commit -m "Update commit"
 "C:\Program Files\Git\cmd\git.exe" push -u origin_momentum_nyse_nasdaq main
-echo git pushå®Œäº†
+echo git pushŠ®—¹
 
 pause
 
